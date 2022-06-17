c9cb105da0f1800a371d8817ec992022805bd19e
윤여빈 <tommy7899@naver.com>
2022-06-18 08:21:31 +0900

    2022-06-18 1st commit

---
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop;
+
+import javax.management.relation.Role;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+
+    }
+
+}
+
